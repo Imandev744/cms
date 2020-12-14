c796ab695f84e40be5cc4da81d3b3ddc01d506eb
iman sadafi <sadafiiman@gmail.com>
2020-12-15 00:15:16 +0330

    rename method index to show

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    public function index()
+    public function showRegisterForm()
     {
         return view('auth.register');
      }
