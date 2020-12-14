95944367e775376a56bd77f346af3e4c0ca73dab
iman sadafi <sadafiiman@gmail.com>
2020-12-15 00:13:38 +0330

    rename and added form to password fome blade

---
+++ resources/views/auth/passwordform.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.interface')
+
+@section('container')
+
+    <div class="container mt-5" >
+        <h2>Form authentication</h2>
+        <form action="{{route('check.num')}}" method="post">
+            @csrf
+
+
+            <div class="form-group">
+                <label for="mobile">Mobile:</label>
+                <input type="text" class="form-control"  placeholder="Enter phone number" name="mobile" required>
+                @error('mobile')
+                <div>
+                    {{$message}}
+                </div>
+                @enderror
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+@endsection
+
+
