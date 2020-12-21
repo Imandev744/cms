4424be68717e7cd5977ca2d88be258179560f013
iman sadafi <sadafiiman@gmail.com>
2020-12-21 20:26:29 +0330

    add a variable to get user obj to check

--- app/Http/Controllers/Auth/RoleController.php
+++ app/Http/Controllers/Auth/RoleController.php
@@ -9,14 +9,15 @@ class RoleController extends Controller
 {
     public function rolecheck()
     {
-        switch(auth()->user()->role){
+        $user=\auth()->user();
+
+
+        switch($user->role){
 
             case 'admin':
                 return redirect()->route('admin.index');
             case 'client':
                 return redirect()->route('user.index');
-
-
         }
     }
 }
